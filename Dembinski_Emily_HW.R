c2e907ae3511337cc493902f38a32e3e7d9641a5
edembinski <116750266+edembinski@users.noreply.github.com>
2022-11-01 08:52:12 -0400

    My Homework for R&R Computational Reproducibility I

---
+++ Dembinski_Emily_HW.R
@@ -0,0 +1,11 @@
+# Homework Computational Reproducibility I 
+
+#I have never coded before so this entire process has been a lot of learning.
+#Through it all I have learned about what GitHub is and have started to familiarize myself with R studio
+#I think I am still a little confused about the whole conceptual layout of everything, but I think that will get sorted as I use it more.
+#The activity provided a good first experience. 
+
+#Wilson et. al. suggest that tracking changes and collaboration are key tenants of "good enough" practices in scientific computing.
+#R and Github help with these suggestions as in combination pushing and pulling R script from Github allows collaboration to occur while keeping track of all the changes.
+#This allows other researchers to better understand one's work and keeps things clear for others that are interested. 
+
